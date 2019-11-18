45aaef9b9a05ec0da46fea070ead0924cd30fd23
Bronson <bronson@haesemathematics.com.au>
2019-11-18 14:42:01 +1030

    tidy cec script

--- cec.py
+++ cec.py
@@ -18,7 +18,7 @@ log_to_file = False
 def has_inhibit():
     bus = dbus.SessionBus()
     pm = bus.get_object("org.freedesktop.PowerManagement", "/org/freedesktop/PowerManagement/Inhibit")
-    return pm.HasInhibit()
+    return bool(pm.HasInhibit())
     
 
 def runCommand(command):
@@ -30,9 +30,9 @@ def runCommand(command):
     return result
 
 
-def is_monitor_on():
+def get_monitor():
     # assume monitor is on if inhibit is enabled/dpms disabled
-	if not is_dpms_enabled():
+	if not get_dpms():
 		return True
     
 	if runCommand("xset -q").find("Monitor is On") != -1:
@@ -41,16 +41,16 @@ def is_monitor_on():
 	return False
 
 
-def is_dpms_enabled():
+def get_dpms():
     if runCommand("xset -q").find("DPMS is Enabled") != -1:
         return True
 		
     return False
 
 
-def send_cec_command(is_monitor_on):
+def send_cec_command(state):
     # commands will fail if cec device not found
-    if (is_monitor_on):
+    if (state):
         log("turning screen on")
         runCommand("echo 'on 0' | cec-client -s")
     else:
@@ -88,60 +88,84 @@ if __name__ == '__main__':
     log(runCommand('lsusb | grep "CEC"'))
 
     set_defaults()
-    prev_monitor_state = is_monitor_on()
+    prev_monitor_state = get_monitor()
     prev_inhibit_state = has_inhibit()
-    prev_dpms_state = is_dpms_enabled()
+    prev_dpms_state = get_dpms()
     max_screen_on_time = 3 * 60 * 60 # hours * minutes * seconds
     screen_on_time = get_time()
     
-    
-    log('startup monitor on: ' + str(prev_monitor_state))
-    log('startup dpms enabled: ' + str(prev_dpms_state))
+    log('initial state:')
+    log('monitor: ' + str(prev_monitor_state))
+    log('dpms: ' + str(prev_dpms_state))
+    log('inhibit: ' + str(prev_inhibit_state))
     log()
+
+    # note:
+    # is inhibit and dpms the same?
     
     while True:
         time.sleep(1)
         
-        # get dpms state
+        # set some variables
+        inhibit_changed = False
+        monitor_changed = False
+        dpms_changed = False
+
+        # get variables for current tick
+        cur_time = get_time()
+        cur_dpms = get_dpms()
+        cur_monitor = get_monitor()
+        cur_inhibit = has_inhibit()
+
+        #
+        # check for state changes
+        #
+
         # if dpms is off, we assume the monitor is on
-        dpms_enabled = is_dpms_enabled()
-        if (dpms_enabled != prev_dpms_state):
-            prev_dpms_state = dpms_enabled
+        if (cur_dpms != prev_dpms_state):
+            prev_dpms_state = cur_dpms
+            dpms_changed = True
             log('dpms state change')
         
         # get monitor state
-        monitor_state_changed = False
-        monitor_on = is_monitor_on()
-        
-        if prev_monitor_state != monitor_on:
-            prev_monitor_state = monitor_on
-            monitor_state_changed = True
+        if (prev_monitor_state != cur_monitor):
+            prev_monitor_state = cur_monitor
+            monitor_changed = True
             log('monitor state change')
             
         # inhibit state
-        inhibit_state_changed = False
-        inhibit_state = has_inhibit()
-        if prev_inhibit_state != inhibit_state:
-            prev_inhibit_state = inhibit_state
-            inhibit_state_changed = True
+        if (prev_inhibit_state != cur_inhibit):
+            prev_inhibit_state = cur_inhibit
+            inhibit_changed = True
             log('inhibit state change')
-            
+
+        #
+        # apply state changes
+        #
+    
         # toggle screen
-        if (dpms_enabled and monitor_state_changed):    
+        # dpms is on, and monitor changed state
+        if (cur_dpms and monitor_changed):    
             # send cec command
-            log("Monitor on: " + str(monitor_on))
-            send_cec_command(monitor_on)
+            log("Monitor on: " + str(cur_monitor))
+            send_cec_command(cur_monitor)
             
         # restart sleep timer
-        if (monitor_state_changed and monitor_on == True):
-            screen_on_time = get_time()
+        # monitor has turned on
+        # monitor changed state and monitor is on 
+        if (monitor_changed and cur_monitor == True):
+            screen_on_time = cur_time
             
         # restart timer
-        # this can only happen if screen is on
-        if inhibit_state_changed:
-            screen_on_time = get_time()
+        # video/app started or stopped
+        # inhibit has changed state
+        if (inhibit_changed):
+            screen_on_time = cur_time
             
-        # screen screen timer
-        if (monitor_on == True and (get_time() - screen_on_time) > max_screen_on_time):
+        # screen timer
+        # if monitor is on and time is larger than max time
+        # turn dpms off
+        # turn off screen will occur next tick
+        if (cur_monitor == True and (cur_time - screen_on_time) > max_screen_on_time):
             log("Force sleep")
             runCommand("xset dpms force off")
