185c07aa7e3b1cb6daec6a09d22786ff6e623771
Fabian <supagu@gmail.com>
2020-10-31 15:41:25 +1030

    github support

--- git_backup.sh
+++ git_backup.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
-# ensure you have appropriate ssh key added to gitlab
+# ensure you have appropriate ssh key added to gitlab and github
+#
+# gitlab:
 # generate a key on the server with:
-#   ssh-keygen -t ed25519 -C "fab server"
+#   ssh-keygen -t ed25519 -C "comment here"
 #   cat /home/server/.ssh/id_ed25519.pub
 #
 # paste results into gitlab account > settings > ssh keys
+#
+# github:
+#   cd ~/.ssh/ && ssh-keygen -t rsa -b 4096 -C "email@example.com"
+#   cat id_rsa.pub
+#
+# paste result into github account > SSH and GPG keys > New SSH key
 
 BACKUP_DIR=~/Projects
-GITLAB_URL="git@gitlab.com:bit-shift-io"
+GITLAB="git@gitlab.com:bit-shift-io"
+GITHUB="git@github.com:bit-shift-io"
 
-function gitlab_backup {
-    DIR="$BACKUP_DIR/$1"
-    REPO_URL="$GITLAB_URL/$1.git"
-    echo "Backing up $REPO_URL"
+function git_backup {
+    DIR="$BACKUP_DIR/$2"
+    REPO_URL="$1/$2.git"
+    #echo $DIR
     if [ ! -d "$DIR" ] 
     then
+        echo "Backing up $REPO_URL. Cloning..."
         cd $BACKUP_DIR
         git clone "$REPO_URL"
     else
+        echo "Backing up $REPO_URL. Fetching..."
         cd $DIR
         git fetch --all
     fi
 }
 
 mkdir $BACKUP_DIR
-gitlab_backup "bitshift-www"
-gitlab_backup "trains-and-things"
-gitlab_backup "dog-fight"
-gitlab_backup "misc"
-gitlab_backup "shop"
+git_backup $GITLAB "bitshift-www"
+git_backup $GITLAB "trains-and-things"
+git_backup $GITLAB "dog-fight"
+git_backup $GITLAB "misc"
+git_backup $GITLAB "shop"
+
+#git_backup $GITHUB "scripts"
+git_backup $GITHUB "macrokey"
+git_backup $GITHUB "qweather"
+git_backup $GITHUB "audiobook"
