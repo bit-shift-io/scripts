c59c94da4036236ba4a945a37f2ebbf18643dcfc
Fabian <supagu@gmail.com>
2021-09-01 20:45:45 +0930

    added tip to shave off another 9 seconds boot time

--- boot_speed_up.sh
+++ boot_speed_up.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo systemctl mask NetworkManager-wait-online.service
+
+# https://forum.manjaro.org/t/slow-firmware-boot-time-on-fresh-install/36678/16
+# edit and change: 
+# /etc/default/grub
+# GRUB_TIMEOUT=10 to 1
+# then:
+# sudo update-grub
