b4b9a108fec9ffbd300b31c99b5af15732f44045
Bronson <bronsonmathews@gmail.com>
2021-11-23 03:34:28 +1030

    webthings to rpi

--- rpi.sh
+++ rpi.sh
@@ -15,16 +15,52 @@ function main {
     server tools
     ===================
     1) zigbee2mqtt service
+    2) adgaurd install
+    3) webthings service
     *) Any key to exit
     :" ans;
     reset
     case $ans in  
         1) fn_zigbee2mqtt ;;
+        2) fn_adguard ;;
+        3) fn_webthings ;;
         *) $SHELL ;;
     esac
     done
 }
 
+
+function fn_webthings {
+    # create service
+sudo tee /etc/systemd/system/webthings.service > /dev/null << EOL
+    [Unit]
+    Description=webthings
+    After=network.target
+
+    [Service]
+    ExecStart=/usr/bin/npm start
+    WorkingDirectory=/home/pi/webthings/gateway
+    StandardOutput=inherit
+    StandardError=inherit
+    Restart=always
+    User=pi
+
+    [Install]
+    WantedBy=multi-user.target
+EOL
+
+    sudo systemctl reset-failed webthings
+    sudo systemctl enable webthings
+    sudo systemctl start webthings
+}
+
+function fn_adguard {
+    wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm.tar.gz
+    tar xvf AdGuardHome_linux_arm.tar.gz
+    rm AdGuardHome_linux_arm.tar.gz
+    cd AdGuardHome
+    sudo ./AdGuardHome -s install
+}
 
 
 function fn_zigbee2mqtt {
