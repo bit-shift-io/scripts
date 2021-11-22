2054e5ab2d5b42fe8fe2e5ec8c9c89408dc93041
Bronson <bronsonmathews@gmail.com>
2021-11-23 03:37:54 +1030

    adguard

--- rpi.sh
+++ rpi.sh
@@ -54,7 +54,9 @@ EOL
     sudo systemctl start webthings
 }
 
+
 function fn_adguard {
+    cd $HOME
     wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm.tar.gz
     tar xvf AdGuardHome_linux_arm.tar.gz
     rm AdGuardHome_linux_arm.tar.gz
