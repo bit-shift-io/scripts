a3a87e3802c0bfd572b180d033c5f9404e937b8a
Bronson <bronsonmathews@gmail.com>
2022-11-09 18:51:04 +1030

    docker backup

--- backup.sh
+++ backup.sh
@@ -3,10 +3,13 @@
 hostname=$(hostname)
 archive=$HOME/docker-${hostname}.tar.gz
 backup=$HOME/Docker
-
 containers=$(docker container list -qa)
+
+echo "stop containers"
 sudo docker container stop ${containers}
 
-sudo tar -c -f -z -v  ${archive} ${backup}
+echo "create backup..."
+sudo tar -c -f -z -v  ${archive} ${backup} > /dev/null
 
+echo "restart containers"
 sudo docker restart ${containers}
