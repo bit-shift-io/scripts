6eeaba11836939c4e801a40f240f5e8bdbc8ef70
Fabian <supagu@gmail.com>
2024-01-24 18:18:17 +1030

    use rsync for local backup

--- fabian_backup.sh
+++ fabian_backup.sh
@@ -11,12 +11,22 @@
 # fi
 
 
-DEST_DIR_1="/run/media/server/backup/backup"
-DEST_DIR_2="$HOME/Backups/Fabian/backup"
+DEST_DIR_1="/run/media/server/backup/backup" # external hdd backup
+DEST_DIR_2="$HOME/Backups/Fabian/backup" # local backup
 
 cd rsync-time-backup
 
-echo "Backup up from '$HOME' to '$DEST_DIR_1' and from '$HOME' to '$DEST_DIR_2'"
+# local backup - just uses rsync
+echo "Performing local backup up from '$HOME' to '$DEST_DIR_2'..."
+rsync -av $HOME/ $DEST_DIR_2 --exclude-from ../fabian_backup_excludes.txt
 
-./rsync_tmbackup.sh $HOME $DEST_DIR_1 ../fabian_backup_excludes.txt
-./rsync_tmbackup.sh $HOME $DEST_DIR_2 ../fabian_backup_excludes.txt
+# external hdd backup - use rsync-time-backup
+if [ -d "$DIRECTORY" ]; then
+    echo "Performing external backup up from '$HOME' to '$DEST_DIR_1'...."
+    #./rsync_tmbackup.sh $HOME $DEST_DIR_1 ../fabian_backup_excludes.txt
+    #./rsync_tmbackup.sh $HOME $DEST_DIR_2 ../fabian_backup_excludes.txt
+else
+    echo "Skipping external backup as '$DEST_DIR_1' does not exist."
+fi
+
+echo "Backup complete."
