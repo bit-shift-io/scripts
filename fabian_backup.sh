aeb4c957c5c81b79750988ed033aee3a770b8d6d
Fabian <supagu@gmail.com>
2024-01-24 16:13:45 +1030

    made backup script fabian specific

--- fabian_backup.sh
+++ fabian_backup.sh
@@ -11,10 +11,12 @@
 # fi
 
 
-DEST_DIR="/run/media/server/backup/backup"
+DEST_DIR_1="/run/media/server/backup/backup"
+DEST_DIR_2="$HOME/Backups/Fabian/backup"
 
 cd rsync-time-backup
 
-echo "Backup up from '$HOME' to '$DEST_DIR'"
+echo "Backup up from '$HOME' to '$DEST_DIR_1' and from '$HOME' to '$DEST_DIR_2'"
 
-./rsync_tmbackup.sh $HOME $DEST_DIR ../backup_excludes.txt
+./rsync_tmbackup.sh $HOME $DEST_DIR_1 ../fabian_backup_excludes.txt
+./rsync_tmbackup.sh $HOME $DEST_DIR_2 ../fabian_backup_excludes.txt
