99ba1f7fd2465f73f7fb5131c569278b7b20f4f5
Fabian <supagu@gmail.com>
2024-01-26 08:51:03 +1030

    simplified excludes

--- fabian_backup.sh
+++ fabian_backup.sh
@@ -12,21 +12,19 @@
 
 
 DEST_DIR_1="/run/media/server/offsite/fabian" # external hdd backup
-#DEST_DIR_2="$HOME/Backups/Fabian/backup" # local backup
 
 cd rsync-time-backup
 
-# local backup - just uses rsync
-#echo "Performing local backup up from '$HOME' to '$DEST_DIR_2'..."
-#rsync -av $HOME/ $DEST_DIR_2 --exclude-from ../fabian_backup_excludes.txt
+sudo tee fabian_backup_excludes.txt > /dev/null << EOL
++ /Fabian/
++ /.config/
++ /Melissa/
++ /Pictures/
++ /Docker/
+- /*
+EOL
 
-# external hdd backup - use rsync-time-backup
-#if [ -d "$DIRECTORY" ]; then
-    echo "Performing external backup up from '$HOME' to '$DEST_DIR_1'...."
-    ./rsync_tmbackup.sh $HOME $DEST_DIR_1 ../fabian_backup_excludes.txt
-    #./rsync_tmbackup.sh $HOME $DEST_DIR_2 ../fabian_backup_excludes.txt
-#else
-#    echo "Skipping external backup as '$DEST_DIR_1' does not exist."
-#fi
+echo "Performing external backup up from '$HOME' to '$DEST_DIR_1'...."
+./rsync_tmbackup.sh $HOME $DEST_DIR_1 fabian_backup_excludes.txt --dry-run
 
 echo "Backup complete."
