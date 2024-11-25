68c57522c2222291d844962ad8bd4c7bfaab5400
Bronson <bronsonmathews@gmail.com>
2024-11-25 20:27:56 +1030

    docker install

--- docker.sh
+++ docker.sh
@@ -152,11 +152,11 @@ function fn_docker_base_debian {
     echo "install docker for debian: $VERSION"
 
     # remove old
-    ./util.sh -u docker.io docker-compose
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
 
     # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt update
+    sudo apt install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -166,10 +166,10 @@ function fn_docker_base_debian {
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+    sudo apt update
 
     # install
-    ./util.sh -i docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # add user
     sudo usermod -aG docker ${USER}
