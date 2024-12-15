687dd9629ea4022accd452cb428162483c0a4bdc
Bronson <bronsonmathews@gmail.com>
2024-12-16 07:03:22 +1030

    docker

--- docker.sh
+++ docker.sh
@@ -176,15 +176,16 @@ function fn_docker_base_debian {
 
     # add user
     sudo usermod -aG docker ${USER}
+    echo "reboot required!"
 }
 
 function fn_docker_base_arch {
-    ./util.sh -i docker 
-    # old? docker-compose
+    ./util.sh -i docker docker-compose
     sudo systemctl enable docker
     sudo systemctl start docker
     # add user
     sudo usermod -aG docker ${USER}
+    echo "reboot required!"
 }
 
 
