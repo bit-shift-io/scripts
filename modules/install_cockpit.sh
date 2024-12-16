268a89aceba546fa6c1293f5edbb4955890ef487
Bronson <bronsonmathews@gmail.com>
2024-12-16 21:18:22 +1030

    cockpit

--- modules/install_cockpit.sh
+++ modules/install_cockpit.sh
@@ -44,18 +44,19 @@ function fn_debian {
 
 # docker
 cd $HOME
-git clone https://github.com/chabad360/cockpit-docker.git
-cd cockpit-docker
-make
-sudo make install
-
+wget -c https://github.com/chabad360/cockpit-docker/releases/download/16/cockpit-docker-16.tar.xz
+sudo tar xf cockpit-docker-16.tar.xz -C /tmp/
+sudo mkdir -p /usr/share/cockpit/docker
+sudo mv /tmp/cockpit-docker/dist/* /usr/share/cockpit/docker
 
 # files, should be available in future repo
 cd $HOME
-git clone https://github.com/cockpit-project/cockpit-files.git
-cd cockpit-files
-make
-sudo make install
+wget -c https://github.com/cockpit-project/cockpit-files/releases/download/13/cockpit-files-13.tar.xz
+sudo tar xf cockpit-files-13.tar.xz -C /tmp/
+sudo mkdir -p /usr/share/cockpit/files
+sudo mv /tmp/cockpit-files/dist/* /usr/share/cockpit/files
+
+sudo rm -r /tmp/*
 
 sudo systemctl enable cockpit.socket --now
 echo "Complete"
