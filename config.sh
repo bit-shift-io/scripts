5deef65e997ed3aab5b7d594eced85bb00bebb43
Bronson <bronsonmathews@gmail.com>
2025-01-02 10:02:07 +1030

    print support

--- config.sh
+++ config.sh
@@ -355,6 +355,10 @@ function fn_base_apps {
     echo -e '\n\nInstalling packages...'
     ./util.sh -i yay base-devel openssh partitionmanager filelight kio-extras plasma-browser-integration libreoffice firefox keepassxc git rustup vulkan-radeon lib32-vulkan-radeon sshfs
     
+    # printer support
+    ./util.sh -i cups cups-pdf system-config-printer avahi
+    sudo systemctl enable cups.service
+    
     # aur software
     echo -e '\n\nInstalling AUR packages...'
     ./util.sh -i visual-studio-code-bin
