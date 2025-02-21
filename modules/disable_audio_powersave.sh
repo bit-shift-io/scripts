9066c0102f3d052027153070003046d3cca8110c
Bronson <bronsonmathews@gmail.com>
2025-02-21 11:41:10 +1030

    diable audio suspend

---
+++ modules/disable_audio_powersave.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
+sudo tee /etc/wireplumber/wireplumber.conf.d/51-disable-suspension.conf > /dev/null << EOL
+monitor.alsa.rules = [
+  {
+    matches = [
+      {
+        # Matches all sources
+        node.name = "~alsa_input.*"
+      },
+      {
+        # Matches all sinks
+        node.name = "~alsa_output.*"
+      }
+    ]
+    actions = {
+      update-props = {
+        session.suspend-timeout-seconds = 0
+      }
+    }
+  }
+]
+# bluetooth devices
+monitor.bluez.rules = [
+  {
+    matches = [
+      {
+        # Matches all sources
+        node.name = "~bluez_input.*"
+      },
+      {
+        # Matches all sinks
+        node.name = "~bluez_output.*"
+      }
+    ]
+    actions = {
+      update-props = {
+        session.suspend-timeout-seconds = 0
+      }
+    }
+  }
+]
+EOL
+#systemctl restart --user pipewire.service
+#systemctl restart --user wireplumber.service
+systemctl restart pipewire.service
+systemctl restart wireplumber.service
