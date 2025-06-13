a51e70f2ddca0c1af3537758a21ad721f3697d1c
Bronson <bronsonmathews@gmail.com>
2025-06-13 20:09:23 +0930

    fixed audio

--- modules/disable_audio_suspend.sh
+++ modules/disable_audio_suspend.sh
@@ -1,60 +1,69 @@
 #!/bin/bash
 
-# requires reboot
+# test
 echo 0 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
+
+# make permanent
+sudo tee /etc/modprobe.d/audio_power_save.conf > /dev/null << EOL
+options snd_hda_intel power_save=0
+EOL
+
+# optional?
+sudo update-initramfs -u
+
 
 
 # https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
-mkdir -p $HOME/.config/wireplumber/wireplumber.conf.d/
+#mkdir -p $HOME/.config/wireplumber/wireplumber.conf.d/
 
 # /etc/wireplumber/wireplumber.conf.d/51-disable-suspension.conf
 # ~/.config/wireplumber/wireplumber.conf.d/51-disable-suspension.conf
-tee $HOME/.config/wireplumber/wireplumber.conf.d/51-disable-suspension.conf > /dev/null << EOL
-monitor.alsa.rules = [
-  {
-    matches = [
-      {
-        # Matches all sources
-        node.name = "~alsa_input.*"
-      },
-      {
-        # Matches all sinks
-        node.name = "~alsa_output.*"
-      }
-    ]
-    actions = {
-      update-props = {
-        session.suspend-timeout-seconds = 0
-        dither.method = "wannamaker3", # add dither of desired shape
-        dither.noise = 2, # add additional bits of noise
-      }
-    }
-  }
-]
-# bluetooth devices
-monitor.bluez.rules = [
-  {
-    matches = [
-      {
-        # Matches all sources
-        node.name = "~bluez_input.*"
-      },
-      {
-        # Matches all sinks
-        node.name = "~bluez_output.*"
-      }
-    ]
-    actions = {
-      update-props = {
-        session.suspend-timeout-seconds = 0
-      }
-    }
-  }
-]
-EOL
+#tee $HOME/.config/wireplumber/wireplumber.conf.d/51-disable-suspension.conf > /dev/null << EOL
+#monitor.alsa.rules = [
+#  {
+#    matches = [
+#      {
+#        # Matches all sources
+#        node.name = "~alsa_input.*"
+#      },
+#      {
+#        # Matches all sinks
+#        node.name = "~alsa_output.*"
+#      }
+#    ]
+#    actions = {
+#      update-props = {
+#        session.suspend-timeout-seconds = 0
+#        dither.method = "wannamaker3", # add dither of desired shape
+#        dither.noise = 2, # add additional bits of noise
+#      }
+#    }
+#  }
+#]
+## bluetooth devices
+#monitor.bluez.rules = [
+#  {
+#    matches = [
+#      {
+#        # Matches all sources
+#        node.name = "~bluez_input.*"
+#      },
+#      {
+#        # Matches all sinks
+#        node.name = "~bluez_output.*"
+#      }
+#    ]
+#    actions = {
+#      update-props = {
+#        session.suspend-timeout-seconds = 0
+#      }
+#    }
+#  }
+#]
+#EOL
 
 
-systemctl restart --user pipewire.service
-systemctl restart --user wireplumber.service
+#systemctl restart --user pipewire.service
+#systemctl restart --user wireplumber.service
 #systemctl restart pipewire.service
 #systemctl restart wireplumber.service
