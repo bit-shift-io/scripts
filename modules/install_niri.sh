53a0a59362f3c252374532815db7a7020527e386
Bronson <bronsonmathews@gmail.com>
2025-12-15 15:32:56 +1030

    niri and tools

---
+++ modules/install_niri.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# fish shell
+# alaricitty shell
+# yazi file manager
+# niri tiling wm
+# brightnessctl laptop display
+./util.sh -i noctalia-shell 
+# fish niri yazi alaricitty brightnessctl
+
+# dank material shell - noctalia is better
+# curl -fsSL https://install.danklinux.com | sh
+
+
+tee ~/.config/fish/config.fish > /dev/null << EOL
+if status is-interactive
+   # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-05-23 00:57:46
+set PATH $PATH $HOME/.local/bin
+
+# yazi exit in current dir
+function y
+   set tmp (mktemp -t "yazi-cwd.XXXXXX")
+   yazi $argv --cwd-file="$tmp"
+   if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+       builtin cd -- "$cwd"
+   end
+   rm -f -- "$tmp"
+end
+EOF
+
+echo "add the following to your ~./bashrc"
+echo 'eval "fish"'
+
+echo "Complete"
