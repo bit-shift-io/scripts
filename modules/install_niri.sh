6a4c63cdbb6d9bf5ae7290873be4cde6765f27b4
Bronson <bronsonmathews@gmail.com>
2025-12-18 10:26:03 +1030

    niri

--- modules/install_niri.sh
+++ modules/install_niri.sh
@@ -4,7 +4,7 @@
 # yazi file manager
 # niri tiling wm
 # brightnessctl laptop display
-./util.sh -i niri noctalia-shell
+./util.sh -i niri noctalia-shell swayidle
 
 # fish niri yazi alaricitty brightnessctl
 
