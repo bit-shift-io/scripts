95c27426ee7f7ee814305cbec7bc24512b165ab9
Fabian Mathews <supagu@gmail.com>
2026-03-28 15:56:54 +1030

    firewall

--- modules/config_firewall.sh
+++ modules/config_firewall.sh
@@ -4,8 +4,14 @@
 #
 # https://www.reddit.com/r/cachyos/comments/1rta90c/kde_connect_phone_and_laptop_not_connecting/
 #
+#
+# KDE Connect
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
+
+# Node red home
+sudo ufw allow 1880/tcp
+
 sudo ufw reload
 
 echo "Done!"
