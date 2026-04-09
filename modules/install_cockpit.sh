76cfb7f4e85663e68f0952ce4183612fdfd389d9
Bronson <bronsonmathews@gmail.com>
2026-04-09 12:39:57 +0930

    backup

--- modules/install_cockpit.sh
+++ modules/install_cockpit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-./util.sh -i cockpit cockpit-system cockpit-podman cockpit-files cockpit-packagekit
+./util.sh -i cockpit cockpit-system cockpit-podman cockpit-files cockpit-packagekit cockpit-storaged
 # extras maybe use of use?
-# cockpit-machines cockpit-sosreport cockpit-networkmanager cockpit-storaged
+# cockpit-machines cockpit-sosreport cockpit-networkmanager
 sudo systemctl enable cockpit.socket --now
 echo "Complete"
