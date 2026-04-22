e640fa46a175b94fecc6329ce0ee15a0328a9869
Bronson <bronsonmathews@gmail.com>
2026-04-22 23:28:51 +0930

    bluetooth speaker

--- modules/config_dietpi.sh
+++ modules/config_dietpi.sh
@@ -37,6 +37,14 @@ function fn_bluetooth_speaker {
     sudo apt update
     sudo apt install -y bluez-alsa-utils bluez-tools
 
+# udev rule to block other ble adapter
+# Only disable the device physically plugged into Bus 1, Port 8 (doesnt have a serial id)
+sudo tee "/etc/udev/rules.d/81-bluetooth-mask.rules" > /dev/null << EOL
+SUBSYSTEM=="usb", KERNELS=="1-8", ATTR{authorized}="0"
+EOL
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
 # Create the Player (Pipes BT audio to your speakers)
 sudo tee /etc/systemd/system/bluealsa-aplay.service > /dev/null << EOL
 [Unit]
@@ -70,12 +78,17 @@ EOL
     # 5. Reload and Start everything
     sudo systemctl daemon-reload
     sudo systemctl enable --now bluealsa bt-agent bluealsa-aplay
-    sudo systemctl restart bluetooth
 
-    # 6. Set Bluetooth Identity
+    sleep 2 # Give udev and services a moment to settle
+    sudo rfkill unblock bluetooth
+    sudo hciconfig hci0 up
+
+    # Set Bluetooth
     echo "Configuring adapter as a Speaker..."
     sudo hciconfig hci0 class 0x240428
     sudo hciconfig hci0 sspmode 1
+
+    sudo bluetoothctl power on
     sudo bluetoothctl discoverable on
     sudo bluetoothctl pairable on
 
