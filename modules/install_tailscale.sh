8b7d5db636c6c32796a3059b7bc0728aa52f3bf4
Bronson <bronsonmathews@gmail.com>
2026-07-12 16:10:04 +0930

    tailscale

--- modules/install_tailscale.sh
+++ modules/install_tailscale.sh
@@ -5,15 +5,12 @@ echo "installing..."
 ./util.sh -i tailscale
 #tailscale up
 #tailscale configure systray --enable-startup=systemd
-systemctl --user daemon-reload
-
-sudo tailscale set --operator=$USER
-sudo systemctl start tailscaled
+sudo systemctl --user daemon-reload
+sudo systemctl enable tailscaled --now
 
 #sudo tailscale up
 sudo tailscale up --accept-routes
-
-
+sudo tailscale set --operator=$USER
 systemctl --user enable --now tailscale-systray
 
 #../util.sh -i ktailctl
