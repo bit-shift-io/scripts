c769399e90048e8ac07987382511e27067c54f7d
Bronson <bronsonmathews@gmail.com>
2026-07-28 17:43:31 +0930

    fish now with remote

--- modules/install_fish.sh
+++ modules/install_fish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # install fish shell
-./util.sh -i fish
+./util.sh -i fish ttyd
 
+# ttyd -p 8000 -W -c admin:YourSecretPass fish
 
 tee ~/.config/fish/config.fish > /dev/null << EOL
 if status is-interactive
@@ -47,5 +48,33 @@ fi
 EOT
     echo "Complete. Restart your terminal to verify."
 fi
+
+
+
+# ttyd service
+echo "What credentials would you like to use for web terminal?"
+read -p "Username: " WEB_USER
+read -sp "Password: " WEB_PASS
+
+mkdir -p $HOME/.config/systemd/user
+
+sudo tee $HOME/.config/systemd/user/ttyd.service > /dev/null << EOL
+[Unit]
+Description=ttyd Web Terminal Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/ttyd -p 8000 -W -c $WEB_USER:$WEB_PASS fish
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=default.target
+EOL
+
+systemctl --user daemon-reload
+systemctl --user enable --now ttyd.service
+loginctl enable-linger $USER
 
 echo "Complete"
